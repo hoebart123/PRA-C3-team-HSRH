16561196a221bfaa3a5175aef203cbddbd4a5140
robin <D312181@edu.curio.nl>
2025-11-18 11:24:55 +0100

    contactpagina gemaakt

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <title>Contactpagina</title>
 </head>
 <body>
-    <h1>Welkom op de homepage</h1>
-    <p>Ga naar onze <a href="{{ route('contact') }}">Contactpagina</a></p>
+    <h1>Contact</h1>
+    <p>Hier kan je ons bereiken.</p>
+    <p><a href="{{ route('home') }}">Terug naar homepage</a></p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');

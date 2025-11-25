8ff95c539e65aca24d8eeea9c113cb12311ecb61
SemOtto <D310596@edu.curio.nl>
2025-11-25 11:23:50 +0100

    Nav bar aangepast

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,16 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <title>{{ env('APP_NAME') }}</title>
 </head>
 <body>
     <header>
         <nav>
-            <a href="../home.blade.php">Home</a>
-            <a href="../toernooi.blade.php">Toernooi</a>
-            <a href="../regel.blade.php">Regels</a>
-            <a href="../manage.blade.php">Beheerders Pagina</a>
-            <a href="../contact.blade.php">Contact</a>
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('toernooi') }}">Toernooi</a>
+            <a href="{{ route('regel') }}">Regels</a>
+            <a href="{{ route('beheerder') }}">Beheerders Pagina</a>
+            <a href="{{ route('contact') }}">Contact</a>
+
+            @guest
+                <a href="{{ route('login') }}">Log in</a>
+                <a href="{{ route('register') }}">Registreer</a>
+            @endguest
+
+            @auth
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a href="#" onclick="event.preventDefault(); this.closest('form').submit();">Log uit</a>
+            </form>
+            @endauth
+
         </nav>
     </header>
     <main>

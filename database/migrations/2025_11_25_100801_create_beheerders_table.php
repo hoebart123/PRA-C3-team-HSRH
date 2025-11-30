86ad370c74a50ba379929a851cff086da68cfffa
SemOtto <D310596@edu.curio.nl>
2025-11-30 22:58:32 +0100

    Nog wat gefixt

--- database/migrations/2025_11_25_100801_create_beheerders_table.php
+++ database/migrations/2025_11_25_100801_create_beheerders_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('naam');
             $table->string('school');
             $table->string('email')->unique();
-            $table->string('wachtwoord');
             $table->timestamps();
         });
     }
